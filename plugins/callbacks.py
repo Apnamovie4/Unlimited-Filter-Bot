e988b77b665cdd6e26198726e9b6daa9b46fb95c
Apnamovie4 <99583057+Apnamovie4@users.noreply.github.com>
2022-02-13 15:11:35 +0530

    Update callbacks.py

--- plugins/callbacks.py
+++ plugins/callbacks.py
@@ -70,7 +70,7 @@ async def cb_handler(client, query):
             [
                 [
                     InlineKeyboardButton(
-                        "SOURCE CODE", url="https://github.com//Unlimited-Filter-Bot")
+                        "SOURCE CODE", url="https://github.com/Apnamovie4/Unlimited-Filter-Bot")
                 ],
                 [
                     InlineKeyboardButton("BACK", callback_data="help_data"),
